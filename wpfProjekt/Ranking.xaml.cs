fdd790a7fb5c8f5a1e2277799b908516916436ee
Miszuu <miszu19@gmail.com>
2016-06-02 19:30:24 +0200

    Dostałem raka frame nie dziala

--- wpfProjekt/Ranking.xaml.cs
+++ wpfProjekt/Ranking.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace wpfProjekt
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Ranking.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Ranking : Window
     {
-        public MainWindow()
+        public Ranking()
         {
             InitializeComponent();
         }
